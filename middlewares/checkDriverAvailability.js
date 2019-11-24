939176e3f786e63f1aff6f1cc51eac043d430ea2
P.C. Ndayisenga <paccy@Pacifiques-MacBook-Pro.local>
2019-11-24 14:29:06 +0200

    Edit checkDriverAvailablity middleware

--- middlewares/checkDriverAvailability.js
+++ middlewares/checkDriverAvailability.js
@@ -5,7 +5,7 @@ const { trip: Trip, driver: Driver } = models;
 const checkRiderAvailability = async (req, res, next) => {
     const driver = await Driver.findByPk(req.body.driver);
     const tripDriver = await Trip.findOne({
-        where: { rider: req.body.driver, is_complete: false }
+        where: { driver: req.body.driver, is_complete: false }
     });
     if (!driver) {
         return res.status(404).json({
